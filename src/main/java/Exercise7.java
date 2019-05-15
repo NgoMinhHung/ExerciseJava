be0c5386d86addaf204189aac79a9379a6e48dcc
Minh Hung <ngominhhung97@gmail.com>
2019-05-15 15:56:08 +0700

    fix successed

--- src/main/java/Exercise7.java
+++ src/main/java/Exercise7.java
@@ -1,8 +1,7 @@
 public class Exercise7 {
     public long SumNumberSequense(int n){
         long sum = 0;
-        if(n < 1) return 0;
-        else for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++){
             sum += i*(i+1);
         }
         return sum;
