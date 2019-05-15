51ede6549a111726316100111284f08e0e21b72c
Minh Hung <ngominhhung97@gmail.com>
2019-05-15 16:22:48 +0700

    fix excercise 8

--- src/main/java/Exercise8.java
+++ src/main/java/Exercise8.java
@@ -1,6 +1,4 @@
 public class Exercise8 {
-    public long calculateFactorial(int n){
-        if(n < 2) return 1;
-        else return (n * calculateFactorial(n-1));
-    }
+    public long calculateFactorial(int n) {
+        return (n < 2)? 1 : n * calculateFactorial(n-1);}
 }
--- src/test/java/Exercise8Test.java
+++ src/test/java/Exercise8Test.java
@@ -5,6 +5,5 @@ import static org.junit.Assert.assertEquals;
 public class Exercise8Test {
     @Test
     public void Test_Exercise8(){
-        assertEquals(new Exercise8().calculateFactorial(5), 120);
-    }
+        assertEquals(new Exercise8().calculateFactorial(5), 120); }
 }
