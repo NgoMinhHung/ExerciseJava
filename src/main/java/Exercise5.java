9f7a92953398840f5761bd4a1497835a335d8afd
Minh Hung <ngominhhung97@gmail.com>
2019-05-17 01:30:51 +0700

    Done Exercise 5

--- src/main/java/Exercise5.java
+++ src/main/java/Exercise5.java
@@ -1,17 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise5 {
-    public void printFibonaciNoAboveN(int n) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+    public String fibonaci(int n) {
+        String s = new String();
         int[] A = new int[n];
         A[0] = 1;
         A[1] = 1;
+        s = s + A[0] + " " + A[1];
         for (int i = 2; i < n; i++) {
             A[i] = A[i - 1] + A[i - 2];
+            if (n >= A[i]) s += " " + A[i];
         }
-        for (int i = 0; i < n; i++) {
-            if (A[i] <= n) System.out.print(A[i] + " ");
-        }
+        return s;
     }
 }
