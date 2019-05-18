c3c160d48c82403bbca74c9a3042e8a0f3060b77
Võ Quang Hòa <voquanghoa@gmail.com>
2019-05-19 01:20:33 +0700

    Update Exercise3Test.java

--- src/test/java/Exercise3Test.java
+++ src/test/java/Exercise3Test.java
@@ -3,11 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise3Test {
-    String s = new String("2 * 2 * 2 * 3 * 5 * 5");
-    String s1 = new String("2 * 2 * 5 * 7");
+
     @Test
     public void Test_Exercise3(){
-        assertEquals(new Exercise3().analysisOfPrimeNumber(600),s);
-        assertEquals(new Exercise3().analysisOfPrimeNumber(140),s1);
+        assertEquals(new Exercise3().analysisOfPrimeNumber(600),"2 * 2 * 2 * 3 * 5 * 5");
+        assertEquals(new Exercise3().analysisOfPrimeNumber(140),"2 * 2 * 5 * 7");
     }
 }
