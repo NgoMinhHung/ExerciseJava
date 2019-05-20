77345b37150197171713fa46cc6028f0c3592e50
Minh Hung <ngominhhung97@gmail.com>
2019-05-20 10:25:42 +0700

    Do Exercise 9

--- src/main/java/Exercise9.java
+++ src/main/java/Exercise9.java
@@ -1,6 +1,12 @@
 public class Exercise9 {
     public float calculatePi(){
-        int i = 1;
-        float pi = 0;
+        int i = 0;
+        float result = 0;
+        while(Math.abs(3.1415 - result) >= 0.0001){
+            if(i % 2 == 0) result += (float) 4/(2 * i + 1 );
+            else result -= (float) 4/(2 * i + 1 );
+            i++;
+        }
+        return result;
     }
 }
