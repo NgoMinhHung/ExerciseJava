884bee2832b6dbf8c0e3cc2ea073c5bf9f16fc1f
Minh Hung <ngominhhung97@gmail.com>
2019-05-20 09:11:09 +0700

    Done2 exerise 4

--- src/main/java/Exercise4.java
+++ src/main/java/Exercise4.java
@@ -1,17 +1,17 @@
 public class Exercise4 {
     public String analysisOfPrimeNumberShort(int n){
         int count;
-        StringBuilder s = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         for (int i =2; i <= n; i++){
             count = 0;
             while(n % i == 0){
                 count++;
                 n /= i;
             }
-            if(count > 0) s.append(i);
-            if(count > 1) s.append("^").append(count);
-            if(n > i && count > 0) s.append(" * ");
+            if(count > 0) result.append(i);
+            if(count > 1) result.append("^").append(count);
+            if(n > i && count > 0) result.append(" * ");
         }
-        return s.toString();
+        return result.toString();
     }
 }
