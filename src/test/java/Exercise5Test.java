1dfbd6f4d465aa7d4c36c8e2f1799401bfcad9db
Minh Hung <ngominhhung97@gmail.com>
2019-05-20 08:57:48 +0700

    Done exercise 5

--- src/test/java/Exercise5Test.java
+++ src/test/java/Exercise5Test.java
@@ -4,11 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise5Test {
-    String s = new String("1 1 2 3 5 8");
-    String s1 = new String("1 1 2 3 5 8 13 21");
     @Test
     public void Test_Excercise5(){
-        assertEquals(new Exercise5().fibonaci(9), s);
-        assertEquals(new Exercise5().fibonaci(25), s1);
+        assertEquals(new Exercise5().fibonaci(9), "1 1 2 3 5 8");
+        assertEquals(new Exercise5().fibonaci(25), "1 1 2 3 5 8 13 21");
     }
 }
