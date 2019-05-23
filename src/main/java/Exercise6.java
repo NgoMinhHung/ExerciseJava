dbe0be5bc0adb321a5c8aad8a85443b40aa29348
Minh Hung <ngominhhung97@gmail.com>
2019-05-23 16:44:33 +0700

    final Exercise 6

--- src/main/java/Exercise6.java
+++ src/main/java/Exercise6.java
@@ -1,22 +1,30 @@
 public class Exercise6 {
     public String solveQuadraticEquation(int a, int b, int c){
-        String s = new String();
-        float x,x1,x2;
-        if(a==0 && b!= 0) s = "x= " + (float)-c/b;
-        if(a == 0 && b == 0 && c != 0) s = "vo nghiem";
-        if(a == 0 &&b == 0 && c == 0) s = "vo so nghiem";
+        String s = "";
+        if(a == 0 && b != 0) {
+            s = "x= " + (float) -c/b;
+        }
+        if(a == 0 && b == 0 && c != 0){
+            s = "vo nghiem";
+        }
+        if(a == 0 &&b == 0 && c == 0){
+            s = "vo so nghiem";
+        }
         else if(a != 0) {
             int delta = b * b - 4 * a * c;
+            float x,x1,x2;
             if (delta > 0){
                 x1 = (float)(-b - Math.sqrt(delta)) / (2 * a);
                 x2 = (float)(-b + Math.sqrt(delta)) / (2 * a);
-                s ="x1= " + x1 + " " +"x2= " + x2;
+                s ="x1= " + x1 + " , " +"x2= " + x2;
             }
             if (delta == 0){
                 x = (float)-b/(2*a);
                 s = "x= " + x;
             }
-            if(delta < 0) s = "vo nghiem";
+            if(delta < 0) {
+                s = "vo nghiem";
+            }
         }
         return s;
     }
--- src/test/java/Exercise6Test.java
+++ src/test/java/Exercise6Test.java
@@ -6,7 +6,7 @@ public class Exercise6Test {
         assertEquals(new Exercise6().solveQuadraticEquation(3, 4, 5),"vo nghiem");
         assertEquals(new Exercise6().solveQuadraticEquation(0, 0, 0),"vo so nghiem");
         assertEquals(new Exercise6().solveQuadraticEquation(0, 4, 5),"x= -1.25");
-        assertEquals(new Exercise6().solveQuadraticEquation(5, 3, -8),"x1= -1.6 x2= 1.0");
+        assertEquals(new Exercise6().solveQuadraticEquation(5, 3, -8),"x1= -1.6 , x2= 1.0");
         assertEquals(new Exercise6().solveQuadraticEquation(1, -2, 1),"x= 1.0");
     }
 }
