df9850dd80041a204d04b8c2aee2b9a309d3af9b
Minh Hung <ngominhhung97@gmail.com>
2019-05-23 14:28:32 +0700

    Done 3 Exercise 15

--- src/main/java/Exercise15.java
+++ src/main/java/Exercise15.java
@@ -4,8 +4,10 @@ public class Exercise15 {
         if(Math.abs(a - b) < c && c < a + b && Math.abs(c - a) < b && b < c + a && Math.abs(b - c) < a && a < b + c){
             result = "La tam giac hop le";
             result += "\nChu vi: " + (a+b+c);
-            if(a == b || a == c || b == c) result += "\nLa tam giac can";
-            if(a == b && b == c) result += "\nLa tam giac deu";
+            if(a == b || a == c || b == c){
+                if(a == b && b == c) result += "\nLa tam giac deu";
+                else result += "\nLa tam giac can";
+            }
             if(a*a == b*b + c*c ||b*b == a*a + c*c||c*c == b*b + a*a) result += "\nLa tam giac vuong";
         }
         else return  "3 canh khong hop le";
--- src/test/java/Exercise15Test.java
+++ src/test/java/Exercise15Test.java
@@ -7,5 +7,7 @@ public class Exercise15Test {
     public void Test_CheckTriangle(){
         assertEquals(new Exercise15().checkTriangle(3,4,5), "La tam giac hop le\nChu vi: 12\nLa tam giac vuong");
         assertEquals(new Exercise15().checkTriangle(1,2,3), "3 canh khong hop le");
+        assertEquals(new Exercise15().checkTriangle(6,6,6), "La tam giac hop le\nChu vi: 18\nLa tam giac deu");
+        assertEquals(new Exercise15().checkTriangle(3,3,4), "La tam giac hop le\nChu vi: 10\nLa tam giac can");
     }
 }
