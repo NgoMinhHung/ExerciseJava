b9e51719b46bce2a6ebbcf9c8732a16c2d58594c
Minh Hung <ngominhhung97@gmail.com>
2019-05-23 16:35:05 +0700

    Done n exercise 9

--- src/main/java/Exercise9.java
+++ src/main/java/Exercise9.java
@@ -2,10 +2,9 @@ public class Exercise9 {
     public float calculatePi(){
         int i = 0;
         float result = 0;
-        while(Math.abs(3.1415 - result) >= 0.0001){
-            if(i % 2 == 0) result += (float) 4/(2 * i + 1 );
-            else result -= (float) 4/(2 * i + 1 );
-            i++;
+        while(Math.abs(Math.PI - result) >= 0.0001){
+            result += (float) 4/(2 * i + 1 ) - (float) 4/(2 * (i + 1) + 1 );
+            i += 2;
         }
         return result;
     }
