e85e2dfb07340c4486e9571be38a3118aee25f09
Minh Hung <ngominhhung97@gmail.com>
2019-05-25 17:34:11 +0700

    Done Exercise 10

--- src/test/java/Exercise10Test.java
+++ src/test/java/Exercise10Test.java
@@ -1,10 +1,11 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Exercise10Test {
     @Test
-    public void Test_Exercise10(){
-        assertTrue(Math.abs(new Exercise10().calculatePi() - 3.1416926635905345) < 0.00001);
+    public void Test_calculatePi(){
+        assertEquals(new Exercise10().calculatePi(), 3.141565741650993, 0.00001);
     }
 }
