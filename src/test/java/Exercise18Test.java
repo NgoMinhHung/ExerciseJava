705da11b42a4c0bbba0429447b691c15507dd0e2
Minh Hung <ngominhhung97@gmail.com>
2019-05-27 14:15:29 +0700

    final Exercise 18

--- src/test/java/Exercise18Test.java
+++ src/test/java/Exercise18Test.java
@@ -4,12 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 public class Exercise18Test {
     @Test
-    public void Test_convertDecimalToBinary(){
-        assertEquals(new Exercise18().convertDecimalToBinary(23),"10111");
-        assertEquals(new Exercise18().convertDecimalToBinary(8),"1000");
-    }
-    @Test
-    public void Test_convertBinaryToDecimal(){
-
+    public void Test_reverseBits(){
+        assertEquals(new Exercise18().reverseBits(23),29);
+        assertEquals(new Exercise18().reverseBits(8),1);
     }
 }
