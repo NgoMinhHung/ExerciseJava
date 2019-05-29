d2ddf79c43a8166324592c52dccc6aa9959f06a0
Minh Hung <ngominhhung97@gmail.com>
2019-05-29 11:18:37 +0700

    fix to codecov 100%

--- src/main/java/Exercise26.java
+++ src/main/java/Exercise26.java
@@ -6,38 +6,14 @@ public class Exercise26 {
 
         int result = 0;
 
-        switch (n){
-            case 0: {
-                result = -1;
-                break;
-            }
-            case 1: {
-                result = arrayList.get(0);
-                break;
-            }
-            case 2: {
-                result = arrayList.get(1);
-                break;
-            }
-            case 3: {
-                result = arrayList.get(2);
-                break;
-            }
-            case 4: {
-                result = arrayList.get(3);
-                break;
-            }
-            case 5: {
-                result = arrayList.get(4);
-                break;
-            }
-            default:{
+        if (n < 1) return -1;
+        if (n <= 5) return arrayList.get(n-1);
+        else {
                 for (int i = 5; i < n; i++) {
                     result = (arrayList.get(i - 5) + arrayList.get(i - 4) + arrayList.get(i - 3) + arrayList.get(i - 2) + arrayList.get(i - 1)) % 10;
                     arrayList.add(result);
                 }
             }
-        }
 
         return result;
     }
