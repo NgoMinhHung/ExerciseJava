8f14e93c415f2787a88157b611908298276624da
Minh Hung <ngominhhung97@gmail.com>
2019-05-30 01:01:25 +0700

    Done fix exercise 26

--- src/main/java/Exercise26.java
+++ src/main/java/Exercise26.java
@@ -2,19 +2,23 @@ import java.util.ArrayList;
 
 public class Exercise26 {
 
-    public int fineNumberCrazyFibonacci(int n, ArrayList<Integer> arrayList){
+    public int fineNumberCrazyFibonacci(int n, int[] arr){
 
         int result = 0;
 
         if (n < 1) return -1;
-        if (n <= 5) return arrayList.get(n-1);
+        if (n <= 5) return arr[n-1];
         else {
-                for (int i = 5; i < n; i++) {
-                    result = (arrayList.get(i - 5) + arrayList.get(i - 4) + arrayList.get(i - 3) + arrayList.get(i - 2) + arrayList.get(i - 1)) % 10;
-                    arrayList.add(result);
-                }
+            for (int i = 6; i <= n; i++) {
+                result = (arr[0] + arr[1] + arr[2] + arr[3] + arr[4]) % 10;
+                arr[0] = arr[1];
+                arr[1] = arr[2];
+                arr[2] = arr[3];
+                arr[3] = arr[4];
+                arr[4] = result;
             }
 
-        return result;
+            return result;
+        }
     }
 }
--- src/test/java/Exercise26Test.java
+++ src/test/java/Exercise26Test.java
@@ -10,12 +10,12 @@ public class Exercise26Test {
 
     @Test
     public void Test_findNumberCrazyFibonacci(){
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(0, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),-1);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),1);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(3, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),3);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(4, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),4);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(5, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),5);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(7, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),9);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(10, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),4);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(0, new int[]{1,2,3,4,5}),-1);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, new int[]{1,2,3,4,5}),1);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(3, new int[]{1,2,3,4,5}),3);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(4, new int[]{1,2,3,4,5}),4);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(5, new int[]{1,2,3,4,5}),5);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(7, new int[]{1,2,3,4,5}),9);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(10, new int[]{1,2,3,4,5}),4);
     }
 }
