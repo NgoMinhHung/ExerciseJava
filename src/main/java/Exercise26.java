de5ea9dccc82a81be91b63037ea556ca1983499e
Minh Hung <ngominhhung97@gmail.com>
2019-05-29 10:42:53 +0700

    Done n Exercise 26

--- src/main/java/Exercise26.java
+++ src/main/java/Exercise26.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Exercise26 {
 
-    public int fineNumberCrazyFibonacci(int a, int b, int c, int d, int e, int n){
+    public int fineNumberCrazyFibonacci(int n, ArrayList<Integer> arrayList){
 
         int result = 0;
 
@@ -10,35 +12,29 @@ public class Exercise26 {
                 break;
             }
             case 1: {
-                result = a;
+                result = arrayList.get(0);
                 break;
             }
             case 2: {
-                result = b;
+                result = arrayList.get(1);
                 break;
             }
             case 3: {
-                result = c;
+                result = arrayList.get(2);
                 break;
             }
             case 4: {
-                result = d;
+                result = arrayList.get(3);
                 break;
             }
             case 5: {
-                result = e;
+                result = arrayList.get(4);
                 break;
             }
             default:{
-                int i = 5;
-                while (i < n){
-                    result = (a + b + c + d + e) % 10;
-                    a = b;
-                    b = c;
-                    c = d;
-                    d = e;
-                    e = result;
-                    i++;
+                for (int i = 5; i < n; i++) {
+                    result = (arrayList.get(i - 5) + arrayList.get(i - 4) + arrayList.get(i - 3) + arrayList.get(i - 2) + arrayList.get(i - 1)) % 10;
+                    arrayList.add(result);
                 }
             }
         }
--- src/test/java/Exercise26Test.java
+++ src/test/java/Exercise26Test.java
@@ -1,17 +1,21 @@
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class Exercise26Test {
+
     @Test
     public void Test_findNumberCrazyFibonacci(){
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 0), -1);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 1), 1);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 2), 2);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 3), 3);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 4), 4);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 5), 5);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 7), 9);
-        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, 2, 3, 4,5, 10), 4);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(0, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),-1);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(1, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),1);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(3, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),3);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(4, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),4);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(5, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),5);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(7, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),9);
+        assertEquals(new Exercise26().fineNumberCrazyFibonacci(10, new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))),4);
     }
 }
