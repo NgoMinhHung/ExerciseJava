6755e95d4f379b4e3049641b7ccdfa19e689c4a8
Minh Hung <ngominhhung97@gmail.com>
2019-05-29 13:59:06 +0700

    Done fix Exercise 29

--- src/test/java/Exercise29Test.java
+++ src/test/java/Exercise29Test.java
@@ -3,5 +3,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise29Test {
-    
+    @Test
+    public void Test_convertRomanToDecimal(){
+        assertEquals(new Exercise29().convertRomanToDecimal("DCCCXCV"), 895);
+        assertEquals(new Exercise29().convertRomanToDecimal("CCXXVIII"), 228);
+    }
 }
