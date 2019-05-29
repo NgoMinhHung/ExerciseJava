1e031c1dd529d7b4862426efa55bb91e4a7adc89
Minh Hung <ngominhhung97@gmail.com>
2019-05-29 16:43:39 +0700

    ?

--- src/main/java/Exercise15.java
+++ src/main/java/Exercise15.java
@@ -1,23 +1,24 @@
 public class Exercise15 {
-    public String checkTriangle(int a, int b, int c){
+
+    public String checkTriangle(int a, int b, int c) {
+
         String result = "";
-        if(a < c + b && c < a + b && b < a + c){
-            if(a*a == b*b + c*c ||b*b == a*a + c*c||c*c == b*b + a*a){
+
+        if (a < c + b && c < a + b && b < a + c) {
+            if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == b * b + a * a) {
                 result += "La tam giac vuong";
-            }
-            else
-            if(a == b || a == c || b == c){
-                if(a == b && b == c){
+            } else if (a == b || a == c || b == c) {
+                if (a == b && b == c) {
                     result += "La tam giac deu";
-                }
-                else result += "La tam giac can";
-            }
-            else{
+                } else result += "La tam giac can";
+            } else {
                 result += "La tam giac thuong";
             }
-            result += "\nChu vi: " + (a+b+c);
+            result += "\nChu vi: " + (a + b + c);
+        } else {
+            return "3 canh khong hop le";
         }
-        else return  "3 canh khong hop le";
+
         return result;
     }
 }
--- src/test/java/Exercise15Test.java
+++ src/test/java/Exercise15Test.java
@@ -6,6 +6,8 @@ public class Exercise15Test {
     @Test
     public void Test_CheckTriangle(){
         assertEquals(new Exercise15().checkTriangle(3,4,5), "La tam giac vuong\nChu vi: 12");
+        assertEquals(new Exercise15().checkTriangle(5,4,3), "La tam giac vuong\nChu vi: 12");
+        assertEquals(new Exercise15().checkTriangle(4,5,3), "La tam giac vuong\nChu vi: 12");
         assertEquals(new Exercise15().checkTriangle(1,2,3), "3 canh khong hop le");
         assertEquals(new Exercise15().checkTriangle(6,6,6), "La tam giac deu\nChu vi: 18");
         assertEquals(new Exercise15().checkTriangle(3,3,4), "La tam giac can\nChu vi: 10");
