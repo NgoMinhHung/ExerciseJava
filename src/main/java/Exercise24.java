8f8e1fb3d95a0019e763e51b337db7451a2ad06c
Minh Hung <ngominhhung97@gmail.com>
2019-05-29 19:30:51 +0700

    Done fix n times

--- src/main/java/Exercise24.java
+++ src/main/java/Exercise24.java
@@ -2,20 +2,19 @@ public class Exercise24 {
 
     public int checkFibonacci(int n) {
 
-        int result = 2;
+        int result = 1;
         int previousValue = 1;
         int currentValue = 1;
-        if (n < 1) return 0;
-        else if (n == 1) return 2;
-        else {
-            int nextFibo = previousValue + currentValue;
-            while (nextFibo <= n) {
-                result++;
-                previousValue = currentValue;
-                currentValue = nextFibo;
-                nextFibo = previousValue + currentValue;
-            }
+        if (n < 1){
+            return 0;
         }
+            while (currentValue <= n) {
+                result++;
+                int tmp = previousValue + currentValue;
+                previousValue = currentValue;
+                currentValue = tmp;
+
+            }
 
         return result;
     }
