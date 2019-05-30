02ac356a405239c2cd87320c69518b74540a0faf
Minh Hung <ngominhhung97@gmail.com>
2019-05-30 14:51:49 +0700

    Finish Exercise 15

--- src/main/java/Exercise15.java
+++ src/main/java/Exercise15.java
@@ -1,24 +1,38 @@
 public class Exercise15 {
 
-    public String checkTriangle(int a, int b, int c) {
+    public boolean checkTriangle(double a, double b, double c) {
+        return (a + b > c);
+    }
 
-        String result = "";
-
-        if (a < c + b && c < a + b && b < a + c) {
-            if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == b * b + a * a) {
-                result += "La tam giac vuong";
-            } else if (a == b || a == c || b == c) {
-                if (a == b && b == c) {
-                    result += "La tam giac deu";
-                } else result += "La tam giac can";
-            } else {
-                result += "La tam giac thuong";
-            }
-            result += "\nChu vi: " + (a + b + c);
-        } else {
-            return "3 canh khong hop le";
+    public String findTypeOfTriangle(double a, double b, double c) {
+        if (!checkTriangle(a, b, c)) {
+            return "Khong phai tam giac";
         }
+        if (a == b && a == c) {
+            return "La tam giac deu";
+        }
+        if (a == b || a == c || b == c) {
+            return "La tam giac can";
+        }
+        if (a * a == (b * b + c * c) || (b * b == (a * a + c * c)) || (c * c == (a * a + b * b))) {
+            return "La tam giac vuong";
+        }
+        return "La tam giac thuong";
+    }
 
-        return result;
+    public double getPerimeter(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    public double getAcreage(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public String getParameterOfTriangle(double a, double b, double c) {
+
+        if (checkTriangle(a, b, c)) {
+            return findTypeOfTriangle(a, b, c) + "\nChu vi: " + getPerimeter(a, b, c) + "\nDien tich: " + getAcreage(a, b, c);
+        }else return findTypeOfTriangle(a, b, c);
     }
 }
--- src/test/java/Exercise15Test.java
+++ src/test/java/Exercise15Test.java
@@ -5,15 +5,14 @@ import static org.junit.Assert.assertEquals;
 public class Exercise15Test {
     @Test
     public void Test_CheckTriangle(){
-        assertEquals(new Exercise15().checkTriangle(3,4,5), "La tam giac vuong\nChu vi: 12");
-        assertEquals(new Exercise15().checkTriangle(5,4,3), "La tam giac vuong\nChu vi: 12");
-        assertEquals(new Exercise15().checkTriangle(4,5,3), "La tam giac vuong\nChu vi: 12");
-        assertEquals(new Exercise15().checkTriangle(1,2,3), "3 canh khong hop le");
-        assertEquals(new Exercise15().checkTriangle(6,6,6), "La tam giac deu\nChu vi: 18");
-        assertEquals(new Exercise15().checkTriangle(3,3,4), "La tam giac can\nChu vi: 10");
-        assertEquals(new Exercise15().checkTriangle(5,4,4), "La tam giac can\nChu vi: 13");
-        assertEquals(new Exercise15().checkTriangle(5,7,5), "La tam giac can\nChu vi: 17");
-        assertEquals(new Exercise15().checkTriangle(5,6,7), "La tam giac thuong\nChu vi: 18");
+        assertEquals(new Exercise15().getParameterOfTriangle(3,4,5), "La tam giac vuong\nChu vi: 12.0\nDien tich: 6.0");
+        assertEquals(new Exercise15().getParameterOfTriangle(5,4,3), "La tam giac vuong\nChu vi: 12.0\nDien tich: 6.0");
+        assertEquals(new Exercise15().getParameterOfTriangle(4,5,3), "La tam giac vuong\nChu vi: 12.0\nDien tich: 6.0");
+        assertEquals(new Exercise15().getParameterOfTriangle(1,2,3), "Khong phai tam giac");
+        assertEquals(new Exercise15().getParameterOfTriangle(6,6,6), "La tam giac deu\nChu vi: 18.0\nDien tich: 15.588457268119896");
+        assertEquals(new Exercise15().getParameterOfTriangle(3,3,4), "La tam giac can\nChu vi: 10.0\nDien tich: 4.47213595499958");
+        assertEquals(new Exercise15().getParameterOfTriangle(5,7,5), "La tam giac can\nChu vi: 17.0\nDien tich: 12.497499749949988");
+        assertEquals(new Exercise15().getParameterOfTriangle(5,6,7), "La tam giac thuong\nChu vi: 18.0\nDien tich: 14.696938456699069");
 
     }
 }
