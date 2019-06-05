49aff58b5633a8406920d226a06b850c595af1fc
Minh Hung <ngominhhung97@gmail.com>
2019-06-05 23:01:38 +0700

    Done Array Exercise 2

---
+++ src/main/java/Array2.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Array2 {
+
+    public long sumThreeLargestNumbers(long[] arr) {
+
+        Arrays.sort(arr);
+
+        return arr[arr.length - 1] + arr[arr.length - 2] + arr[arr.length - 3];
+    }
+}
