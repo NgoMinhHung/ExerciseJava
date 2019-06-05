163c2aa24a1f423d2899ec8a2b61f6f6b8532423
Minh Hung <ngominhhung97@gmail.com>
2019-06-05 23:15:55 +0700

    Done Array Exercise 3

---
+++ src/main/java/Array3.java
@@ -0,0 +1,16 @@
+public class Array3 {
+
+    public long sumEvenMinusOddNumbers(long[] arr){
+
+        long result = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 == 0){
+                result += arr[i];
+            } else {
+                result -= arr[i];
+            }
+        }
+        return result;
+    }
+}
