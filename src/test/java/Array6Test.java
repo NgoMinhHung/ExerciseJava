88496fb39a0318fd9356c77fced95abb45de6682
Minh Hung <ngominhhung97@gmail.com>
2019-06-07 15:48:00 +0700

    Done Array Exercise 6

--- src/test/java/Array6Test.java
+++ src/test/java/Array6Test.java
@@ -1,12 +1,13 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class Array6Test {
 
     @Test
     public void Test_sortArray(){
-        assertEquals(new Array6().sortArray(new int[]{14, 11, 10, 6, 1, 4, 3, 2, 7, 9}), new int[]{2, 11, 4, 6, 9, 7, 10, 3, 14, 1});
-        //assertEquals(new Array6().sortArray(new int[]{0,1,2,3,4,5,6,7,8,9,10}),new int[]{0,9,2,7,4,5,6,3,8,1,10});
+        assertArrayEquals(new Array6().sortArray(new int[]{14, 11, 10, 6, 1, 4, 3, 2, 7, 9}),new int[]{2, 11, 4, 6, 9, 10, 7, 14, 3, 1});
+        assertArrayEquals(new Array6().sortArray(new int[]{0,1,2,3,4,5,6,7,8,9,10}),new int[]{0,9,2,7,4,5,6,3,8,1,10});
     }
 }
