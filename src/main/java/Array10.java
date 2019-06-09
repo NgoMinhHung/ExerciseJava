2ba80840fc3738e219d410d7f3f0f1925e4bfaed
Minh Hung <ngominhhung97@gmail.com>
2019-06-09 21:13:26 +0700

    not yet Array Exercise 10

--- src/main/java/Array10.java
+++ src/main/java/Array10.java
@@ -3,16 +3,18 @@ import java.util.ArrayList;
 public class Array10 {
 
     public ArrayList<ArrayList<Integer>> devideEquallyElements(int n, int[] arr) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
 
         if (arr.length == 0) return result;
         if (n > arr.length) {
             return result;
         } else {
             int lengthArr = arr.length;
+
             // tinh toan do dai moi mang con
             int chunkSize = lengthArr / n;
             chunkSize += lengthArr % n != 0 ? 1 : 0;
+
             for(int i = 0; i < lengthArr; i+=chunkSize) {
                 // sau do minh lay tu cac phan tu tu i -> i+chunkSize
                 ArrayList<Integer> splitList = new ArrayList<Integer>();
--- src/test/java/Array10Test.java
+++ src/test/java/Array10Test.java
@@ -3,13 +3,15 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class Array10Test {
 
     @Test
     public void Test_devideEquallyElements(){
-        assertEquals(new Array10().devideEquallyElements(3,new int[]{1,2,3,4,5,6,7,8,9,10}),new int[][]{Arrays.asList((1,2,3,4),(5,6,7,8),(9,10))});
+       assertArrayEquals(new Array10().devideEquallyElements(3,new int[]{1,2,3,4,5,6,7,8,9,10}),new ArrayList<Integer>().toArray(){[1,2,3,4],[5,6,7,8],[9,10]});
 //        assertEquals(new Array10().devideEquallyElements(15,new int[]{1,2,3,4,5,6,7,8,9,10}),new int[][]{});
+//        assertArrayEquals(new Array10().devideEquallyElements(3, new int[]{1,2,3,4,5,6,7,8,9,10}),new ArrayList<ArrayList<Integer>>()(Arrays.asList(Arrays.asList(1,2,3,4),Arrays.asList(5,6,7,8),Arrays.asList(9,10)));
+   //     assertArrayEquals(new Array10().devideEquallyElements(3,new int[]{1,2,3,4,5,6,7,8,9,10}),);
     }
 }
