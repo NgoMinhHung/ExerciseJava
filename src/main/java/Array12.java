b32ebcb75f1360dd658c751f5de48d70e926754d
Minh Hung <ngominhhung97@gmail.com>
2019-06-11 22:34:26 +0700

    fix code

--- src/main/java/Array12.java
+++ src/main/java/Array12.java
@@ -8,7 +8,6 @@ public class Array12 {
         int temp = 0;
         ArrayList<Integer> arrayList = new ArrayList<>();
 
-        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] <= n) {
                 arrayList.add(arr[i]);
--- src/test/java/Array12Test.java
+++ src/test/java/Array12Test.java
@@ -8,6 +8,6 @@ import static org.junit.Assert.assertEquals;
 public class Array12Test {
     @Test
     public void Test_insertNumberIntoArrayAscending(){
-        assertEquals(new Array12().insertNumberIntoArrayAscending(new int[]{1,6,2,4,98,3,6,54,32},11),new ArrayList<>(Arrays.asList(1,2,3,4,6,6,11,32,54,98)));
+        assertEquals(new Array12().insertNumberIntoArrayAscending(new int[]{1,2,3,4,6,6,32,54,98},11),new ArrayList<>(Arrays.asList(1,2,3,4,6,6,11,32,54,98)));
     }
 }
