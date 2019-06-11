dec2e6432571892a09777710a604235e24243d01
Minh Hung <ngominhhung97@gmail.com>
2019-06-12 00:00:25 +0700

    fix code

--- src/main/java/Array9.java
+++ src/main/java/Array9.java
@@ -3,28 +3,28 @@ import java.util.Arrays;
 public class Array9 {
 
     public int binarySearch(int[] arr, int x) {
-        int result = -1;
+
         int l = 0;
         int r = arr.length;
 
-        if (arr.length  == 0) return result;
-        if (x < arr[l]) return result;
-        if (x > arr[r - 1]) return result;
+        if (arr.length == 0) return -1;
+        if (x < arr[l]) return -1;
+        if (x > arr[r - 1]) return -1;
 
-            while (r >= l) {
-                int mid = l + (r - l) / 2;
+        while (r >= l) {
+            int mid = (l + r) / 2;
 
-                if (arr[mid] == x) {
-                    result = mid;
-                }
-
-                if (arr[mid] > x) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+            if (arr[mid] == x) {
+                return mid;
             }
 
-        return result;
+            if (arr[mid] > x) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return -1;
     }
 }
