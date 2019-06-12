50ea148f4a644ecc04f1991c4a1af9db5c1872f1
Minh Hung <ngominhhung97@gmail.com>
2019-06-12 16:34:08 +0700

    Done Array Exercise 19

---
+++ src/main/java/Array19.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class Array19 {
+
+    public int[] convertMatrixOneDimentionalArray(int n, int m, int[][] matrix) {
+
+        int[] result = new int[n * m];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[count] = matrix[i][j];
+                count++;
+            }
+        }
+        return result;
+    }
+
+    public int[][] sortSpiralMatrix(int n, int m, int[][] matrix) {
+
+        int[] oneDimentionalArray = convertMatrixOneDimentionalArray(n, m, matrix);
+        Arrays.sort(oneDimentionalArray);
+        int count = 0;
+        int[][] result = new int[n][m];
+
+        int startingRow = 0;
+        int startingColumn = 0;
+
+        while (startingRow < n && startingColumn < m) {
+
+            for (int i = startingColumn; i < m; i++) {
+                result[startingRow][i] = oneDimentionalArray[count];
+                count++;
+            }
+            startingRow++;
+            for (int i = startingRow; i < n; i++) {
+                result[i][m - 1] = oneDimentionalArray[count];
+                count++;
+            }
+            m--;
+            if (startingRow < n) {
+                for (int i = m - 1; i >= startingColumn; i--) {
+                    result[n - 1][i] = oneDimentionalArray[count];
+                    count++;
+                }
+                n--;
+            }
+            if (startingColumn < m){
+                for (int i = n-1; i >= startingRow; i--){
+                    result[i][startingColumn] = oneDimentionalArray[count];
+                    count++;
+                }
+                startingColumn++;
+            }
+
+        }
+        return result;
+    }
+}
---
+++ src/test/java/Array19Test.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class Array19Test {
+
+    @Test
+    public void Test_sortSpiralMatrix(){
+        assertArrayEquals(new Array19().sortSpiralMatrix(3,5,new int[][]{{5,2,4,1,3},{6,9,8,7,10},{14,12,13,15,11}}),new int[][]{{1,2,3,4,5},{12,13,14,15,6},{11,10,9,8,7}});
+    }
+    @Test
+    public void Test_convertMatrixOneDimentionalArray(){
+        assertArrayEquals(new Array19().convertMatrixOneDimentionalArray(2,3,new int[][]{{1,2,3},{4,5,6}}),new int[]{1,2,3,4,5,6});
+    }
+
+}
