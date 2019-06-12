57236c8af08882f3b0b3d0d63207bb3d661a0c27
Minh Hung <ngominhhung97@gmail.com>
2019-06-12 08:32:52 +0700

    Doing 2 array 7

--- src/main/java/Array7.java
+++ src/main/java/Array7.java
@@ -4,12 +4,12 @@ import java.util.LinkedHashMap;
 
 public class Array7 {
 
-    public int getPositionElementMaximum(int[] arr){
+    public int getPositionElementMaximum(int[] arr) {
 
         int result = 0;
         int Max = arr[0];
 
-        for (int i = 1; i < arr.length; i++){
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] > Max) {
                 Max = arr[i];
                 result = i;
@@ -17,6 +17,7 @@ public class Array7 {
         }
         return result;
     }
+
     public ArrayList<Integer> getLongestArrayOfChildren(int[] arr) {
 
         ArrayList<Integer> result = new ArrayList<>();
@@ -24,16 +25,15 @@ public class Array7 {
         int[] lastPosition = new int[arr.length];   // Mang luu vi tri cuoi cua mang con tuong ung vi tri i
 
         for (int i = 0; i < arr.length; i++) {
-            arrayOfChildren[i] = 1;
-            for (int j = 0; j < i - 1; j++) {
-                if (arr[j] <= arr[i] && arrayOfChildren[i] < arrayOfChildren[j] + 1) {
-                    arrayOfChildren[i] = arrayOfChildren[j] + 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] <= arr[j + 1] && arrayOfChildren[j + 1] < arrayOfChildren[j] + 1) {
+                    arrayOfChildren[j] = arrayOfChildren[j] + 1;
                     lastPosition[i] = j;
                 }
             }
         }
         int positionArrayLongest = getPositionElementMaximum(arrayOfChildren);
-        for (int i = lastPosition[positionArrayLongest]; i <= arrayOfChildren[positionArrayLongest] ; i++){
+        for (int i = lastPosition[positionArrayLongest]; i <= arrayOfChildren[positionArrayLongest]; i++) {
             result.add(arr[i]);
         }
         return result;
