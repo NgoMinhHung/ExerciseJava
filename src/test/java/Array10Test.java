3a0bb66ecffc647d7fdda48996a4d69d25ef73cb
Minh Hung <ngominhhung97@gmail.com>
2019-06-14 14:58:01 +0700

    Test codecov

--- src/test/java/Array10Test.java
+++ src/test/java/Array10Test.java
@@ -11,6 +11,7 @@ public class Array10Test {
     public void Test_devideEquallyElements(){
        assertArrayEquals(new Array10().devideEquallyElements(3,new int[]{1,2,3,4,5,6,7,8,9,10}),new int[][]{{1,2,3,4},{5,6,7,8},{9,10,0,0}});
         assertArrayEquals(new Array10().devideEquallyElements(15,new int[]{1,2,3,4,5,6,7,8,9,10}),null);
+        assertArrayEquals(new Array10().devideEquallyElements(15,new int[]{}),null);
         assertArrayEquals(new Array10().devideEquallyElements(2,new int[]{1,2,3,4,5,6,7,8,9,10}),new int[][]{{1,2,3,4,5},{6,7,8,9,10}});
     }
 }
