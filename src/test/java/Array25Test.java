f8bf0bea8852a3b9cf5bdba018a8f084b131ba8f
Minh Hung <ngominhhung97@gmail.com>
2019-06-14 14:40:22 +0700

    Test codecov

--- src/test/java/Array25Test.java
+++ src/test/java/Array25Test.java
@@ -9,4 +9,8 @@ public class Array25Test {
         assertEquals(new Array25().getGreatestCommonDivisorOfArray(new int[]{5,10,15,20}),5);
         assertEquals(new Array25().getGreatestCommonDivisorOfArray(new int[]{16,12,13,7}),1);
     }
+    @Test
+    public void Test_getGreatestCommonDivisor(){
+        assertEquals(new Array25().getGreatestCommonDivisor(0,13),13);
+    }
 }
