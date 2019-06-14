6b646789b23cc0bf51be4d1e8af766db21d51109
Minh Hung <ngominhhung97@gmail.com>
2019-06-14 14:13:00 +0700

    Done Array Exercise 24

---
+++ src/test/java/Array24Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Array24Test {
+
+    @Test
+    public void Test_getLeastCommonMultiple(){
+        assertEquals(new Array24().getLeastCommonMultiple(new int[]{5,10,15,20}),60);
+        assertEquals(new Array24().getLeastCommonMultiple(new int[]{15,30,12,10}),60);
+    }
+}
