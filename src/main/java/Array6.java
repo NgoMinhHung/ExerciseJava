dd98d9d418cf103c951f8e911a5abf75aebff494
Minh Hung <ngominhhung97@gmail.com>
2019-06-15 13:52:49 +0700

    Done creat function swap

--- src/main/java/Array6.java
+++ src/main/java/Array6.java
@@ -3,19 +3,26 @@ import java.util.Arrays;
 
 public class Array6 {
 
-    public int[] sortArray(int[] arr) {
+    public static void swap(Integer[] arr, Integer pos1, Integer pos2){
+        Integer temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+    public Integer[] sortArray(Integer[] arr) {
 
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] % 2 == 0) {
                 for (int j = i + 1; j < arr.length; j++) {
                     if (arr[j] % 2 == 0 && arr[i] > arr[j]) {
-                        arr[i] += (arr[j] - (arr[j] = arr[i]));
+                        //arr[i] += (arr[j] - (arr[j] = arr[i]));
+                        swap(arr, i,j);
                     }
                 }
             } else {
                 for (int j = i + 1; j < arr.length; j++) {
                     if (arr[j] % 2 != 0 && arr[i] < arr[j]) {
-                        arr[i] += (arr[j] - (arr[j] = arr[i]));
+                        //arr[i] += (arr[j] - (arr[j] = arr[i]));
+                        swap(arr, i,j);
                     }
                 }
             }
