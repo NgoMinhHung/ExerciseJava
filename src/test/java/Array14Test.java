cfacdc2d59d9ec9f7b37ff0826a2e88ce0ee583b
Minh Hung <ngominhhung97@gmail.com>
2019-06-15 14:53:23 +0700

    Test codecov array Exercise 14

--- src/test/java/Array14Test.java
+++ src/test/java/Array14Test.java
@@ -10,5 +10,10 @@ public class Array14Test {
     @Test
     public void Test_convertTwoDimensionalArraytoOneDimensionalArray(){
         assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(3,5, new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}}), new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,10,15,14,13,12,11,6,7,8,9)));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(0,5, new int[][]{{}}), new ArrayList<Integer>(Arrays.asList()));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(3,0, new int[][]{{}}), new ArrayList<Integer>(Arrays.asList()));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(0,0, new int[][]{{}}), new ArrayList<Integer>(Arrays.asList()));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(1,5, new int[][]{{1,2,3,4,5}}), new ArrayList<Integer>(Arrays.asList(1,2,3,4,5)));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(3,1, new int[][]{{1},{6},{11}}), new ArrayList<Integer>(Arrays.asList(1,6,11)));
     }
 }
