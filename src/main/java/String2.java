e663ab9d9fe968b5be17bd8b53a72806f97d0935
Minh Hung <ngominhhung97@gmail.com>
2019-06-16 00:36:42 +0700

    Done String Exercise 2

---
+++ src/main/java/String2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class String2 {
+
+    public ArrayList<String> getWords(String input){
+
+        ArrayList<String> result = new ArrayList<>();
+        String[] temp = input.trim().split("[\\s]");
+
+        for (String word : temp){
+            if (word.matches("[\\w]+")){
+                result.add(word);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/String2Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class String2Test {
+
+    @Test
+    public void Test_getWords(){
+        assertEquals(new String2().getWords("Hung danh bida co 102"), new ArrayList<String>(Arrays.asList("Hung","danh","bida","co","102")));
+        assertEquals(new String2().getWords(""), new ArrayList<String>(){});
+    }
+}
