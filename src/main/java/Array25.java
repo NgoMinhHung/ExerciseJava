7dc6148ae6f786b15e64d4fadd6675b6920c532c
Minh Hung <ngominhhung97@gmail.com>
2019-06-15 23:01:02 +0700

    Done format code

--- src/main/java/Array25.java
+++ src/main/java/Array25.java
@@ -1,18 +1,19 @@
 public class Array25 {
 
-    public  int getGreatestCommonDivisor(int x, int y){
-        if(x*y == 0) return x+y;
-        while(x != y){
-            if(x > y) x = x - y;
-            else y = y - x; }
+    public int getGreatestCommonDivisor(int x, int y) {
+        if (x * y == 0) return x + y;
+        while (x != y) {
+            if (x > y) x = x - y;
+            else y = y - x;
+        }
         return x;
     }
 
-    public int getGreatestCommonDivisorOfArray(int[] arr){
+    public int getGreatestCommonDivisorOfArray(int[] arr) {
 
-        int result = getGreatestCommonDivisor(arr[0],arr[1]);
-        for (int i = 2; i < arr.length; i++){
-            result = getGreatestCommonDivisor(result,arr[i]);
+        int result = getGreatestCommonDivisor(arr[0], arr[1]);
+        for (int i = 2; i < arr.length; i++) {
+            result = getGreatestCommonDivisor(result, arr[i]);
         }
         return result;
     }
