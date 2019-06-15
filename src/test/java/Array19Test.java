e30372de896626c67659a8bc1ff0a541db224601
Minh Hung <ngominhhung97@gmail.com>
2019-06-15 14:28:05 +0700

    Test codecov

--- src/test/java/Array19Test.java
+++ src/test/java/Array19Test.java
@@ -8,6 +8,7 @@ public class Array19Test {
     @Test
     public void Test_sortSpiralMatrix(){
         assertArrayEquals(new Array19().sortSpiralMatrix(3,5,new int[][]{{5,2,4,1,3},{6,9,8,7,10},{14,12,13,15,11}}),new int[][]{{1,2,3,4,5},{12,13,14,15,6},{11,10,9,8,7}});
+        assertArrayEquals(new Array19().sortSpiralMatrix(0,0,new int[][]{}),new int[][]{});
     }
     @Test
     public void Test_convertMatrixOneDimentionalArray(){
