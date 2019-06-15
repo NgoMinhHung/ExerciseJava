e042c3937c4439772bb811f62c8538142d288a06
Minh Hung <ngominhhung97@gmail.com>
2019-06-15 23:07:39 +0700

    Done fix code

--- src/main/java/Array26.java
+++ src/main/java/Array26.java
@@ -1,23 +1,21 @@
 public class Array26 {
 
-    public int reverse(int a){
+    public int reverse(int a) {
 
         int result = 0;
 
-        while (a != 0){
-            result += a % 10;
+        while (a != 0) {
+            result = 10 * result + a % 10;
             a /= 10;
-            if (a != 0){
-                result *= 10;
-            }
         }
         return result;
     }
-    public int sumNumberReverseOfArray(int[] arr){
+
+    public int sumNumberReverseOfArray(int[] arr) {
 
         int result = 0;
 
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             result += reverse(arr[i]);
         }
         return result;
