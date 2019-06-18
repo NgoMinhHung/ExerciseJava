ba1f558e1aaf13fd4e292369dbf2235293a33725
Minh Hung <ngominhhung97@gmail.com>
2019-06-18 13:53:01 +0700

    Done fix code array 20

--- src/main/java/Array20.java
+++ src/main/java/Array20.java
@@ -3,16 +3,11 @@ public class Array20 {
     public int[][] getSubArray(int[][] arr, int startRow,int startCol, int rowCount, int colCount){
 
         int[][] result = new int[rowCount][colCount];
-        int r = 0, c = 0;
 
         if (startRow + colCount <= arr[0].length  && startCol + rowCount <= arr.length ){
-            for (int i = startCol; i < startCol + rowCount; i++){
-                for (int j = startCol; j < startRow + colCount; j++){
-                    result[r][c++] = arr[i][j];
-                }
-                c = 0;
-                r++;
-            }
+            for(int i=0;i<rowCount; i++)
+                for(int j=0; j< colCount; j++)
+                    result[i][j] = arr[i+startRow][j+ startCol];
         }else {
             return null;
         }
