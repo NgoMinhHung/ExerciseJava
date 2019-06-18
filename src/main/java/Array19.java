0230c32911387dd4bdf14dcf93f54edbc2d431db
Minh Hung <ngominhhung97@gmail.com>
2019-06-18 17:04:38 +0700

    Done

--- src/main/java/Array19.java
+++ src/main/java/Array19.java
@@ -2,10 +2,8 @@ import java.util.Arrays;
 
 public class Array19 {
 
-    public int[] convertMatrixOneDimentionalArray(int[][] matrix) {
+    public int[] convertMatrixOneDimentionalArray(int n, int m, int[][] matrix) {
 
-        int n = matrix.length;
-        int m = matrix[0].length;
         int[] result = new int[n * m];
         int count = 0;
         if (n * m != 0) {
@@ -28,44 +26,41 @@ public class Array19 {
 
         int startingRow = 0;
         int startingColumn = 0;
-        if (n * m != 0) {
-            int[] oneDimentionalArray = convertMatrixOneDimentionalArray(matrix);
-            Arrays.sort(oneDimentionalArray);
+        int[] oneDimentionalArray = convertMatrixOneDimentionalArray(n, m, matrix);
+        Arrays.sort(oneDimentionalArray);
 
-            while (startingRow < n && startingColumn < m) {
-                if (startingRow < n) {
-                    for (int i = startingColumn; i < m; i++) {
-                        result[startingRow][i] = oneDimentionalArray[count];
-                        count++;
-                    }
-                    startingRow++;
+        while (startingRow < n && startingColumn < m) {
+            if (startingRow < n) {
+                for (int i = startingColumn; i < m; i++) {
+                    result[startingRow][i] = oneDimentionalArray[count];
+                    count++;
                 }
-                if (startingColumn < m) {
-                    for (int i = startingRow; i < n; i++) {
-                        result[i][m - 1] = oneDimentionalArray[count];
-                        count++;
-                    }
-                    m--;
-                }
-                if (startingRow < n) {
-                    for (int i = m - 1; i >= startingColumn; i--) {
-                        result[n - 1][i] = oneDimentionalArray[count];
-                        count++;
-                    }
-                    n--;
-                }
-                if (startingColumn < m) {
-                    for (int i = n - 1; i >= startingRow; i--) {
-                        result[i][startingColumn] = oneDimentionalArray[count];
-                        count++;
-                    }
-                    startingColumn++;
-                }
-
+                startingRow++;
             }
-        } else{
-            return result;
+            if (startingColumn < m) {
+                for (int i = startingRow; i < n; i++) {
+                    result[i][m - 1] = oneDimentionalArray[count];
+                    count++;
+                }
+                m--;
+            }
+            if (startingRow < n) {
+                for (int i = m - 1; i >= startingColumn; i--) {
+                    result[n - 1][i] = oneDimentionalArray[count];
+                    count++;
+                }
+                n--;
+            }
+            if (startingColumn < m) {
+                for (int i = n - 1; i >= startingRow; i--) {
+                    result[i][startingColumn] = oneDimentionalArray[count];
+                    count++;
+                }
+                startingColumn++;
+            }
+
         }
         return result;
     }
+
 }
