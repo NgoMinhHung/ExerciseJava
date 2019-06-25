00969dd6af78e94b932461a50226d34123667d64
Minh Hung <ngominhhung97@gmail.com>
2019-06-25 10:03:03 +0700

    Done String Exercise 13

---
+++ src/main/java/String13.java
@@ -0,0 +1,26 @@
+public class String13 {
+
+    public boolean isEqualsubString(String inp1,String inp2){
+
+        String str1 = inp1.toLowerCase();
+        String str2 = inp2.toLowerCase();
+
+
+        int index;
+
+        for (int i = 0; i < str2.length(); i++) {
+            index = str1.indexOf(str2.charAt(i));
+            if (index == -1) {
+                return false;
+            }
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            index = str2.indexOf(str1.charAt(i));
+            if (index == -1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
