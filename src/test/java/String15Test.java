004aca8075f8458f49ea75f8472a9249fec5bb74
Minh Hung <ngominhhung97@gmail.com>
2019-06-25 11:38:19 +0700

    Test cov

--- src/test/java/String15Test.java
+++ src/test/java/String15Test.java
@@ -7,6 +7,7 @@ public class String15Test {
     @Test
     public void Test_getnumberVerify(){
         assertEquals(new String15().getnumberVerify("893850597419"),4);
+        assertEquals(new String15().getnumberVerify("893957597419"),0);
         assertEquals(new String15().getnumberVerify("8-d850597419"),-1);
         assertEquals(new String15().getnumberVerify("8safsafd9719"),-1);
     }
