588ab13c177198cb1a032024499a9a4255c5d0cd
Minh Hung <ngominhhung97@gmail.com>
2019-06-25 08:37:14 +0700

    Done String Exercise 9

---
+++ src/test/java/String9Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class String9Test {
+
+    @Test
+    public void Test_convert(){
+        assertEquals(new String9().convert("abcccceeeeeefd"),"abc4e6fd");
+        assertEquals(new String9().convert("abbbbbbbbbbbbbc"),"ab13c");
+        assertEquals(new String9().convert("bbbbbbbbbbbbbbb"),"b15");
+        assertEquals(new String9().convert("abc21sda1"),"Error");
+    }
+}
