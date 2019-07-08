c53a3c13ec2ef176e127e20ddfdc92d4926faec6
Minh Hung <ngominhhung97@gmail.com>
2019-07-08 18:51:02 +0700

    done

--- src/main/java/Array20.java
+++ src/main/java/Array20.java
@@ -1,14 +1,14 @@
 public class Array20 {
 
-    public int[][] getSubArray(int[][] arr, int startRow,int startCol, int rowCount, int colCount){
+    public int[][] getSubArray(int[][] arr, int startRow, int startCol, int rowCount, int colCount) {
 
         int[][] result = new int[rowCount][colCount];
 
-        if (startRow + colCount <= arr[0].length  && startCol + rowCount <= arr.length ){
-            for(int i=0;i<rowCount; i++)
-                for(int j=0; j< colCount; j++)
-                    result[i][j] = arr[i+startRow][j+ startCol];
-        }else {
+        if (startRow + colCount <= arr[0].length && startCol + rowCount <= arr.length) {
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < colCount; j++)
+                    result[i][j] = arr[i + startRow][j + startCol];
+        } else {
             return null;
         }
         return result;
--- src/test/java/Array20Test.java
+++ src/test/java/Array20Test.java
@@ -1,15 +1,54 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class Array20Test {
 
     @Test
     public void Test_getSubArray(){
-        assertArrayEquals(new Array20().getSubArray(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}},1,1,3,3),new int[][]{{7,8,9},{12,13,14},{17,18,19}});
-        assertArrayEquals(new Array20().getSubArray(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}},1,1,5,3),null);
-        assertArrayEquals(new Array20().getSubArray(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}},1,1,3,5),null);
-        assertArrayEquals(new Array20().getSubArray(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}},1,1,5,5),null);
-        assertArrayEquals(new Array20().getSubArray(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}},1,1,2,3),new int[][]{{7,8,9},{12,13,14}});
+        int [][]arr = {
+                        {1,     2,      3,      4,      5},
+                        {6,     7,      8,      9,      10},
+                        {11,    12,     13,     14,     15},
+                        {16,    17,     18,     19,     20}};
+        assertArrayEquals(new Array20().getSubArray(arr,1,1,3,3),new int[][]{{7,8,9},{12,13,14},{17,18,19}});
     }
+    @Test
+    public void Test_getSubArray2(){
+        int [][]arr = {
+                        {1,     2,      3,      4,      5},
+                        {6,     7,      8,      9,      10},
+                        {11,    12,     13,     14,     15},
+                        {16,    17,     18,     19,     20}};
+        assertArrayEquals(new Array20().getSubArray(arr,1,1,5,3),null);
+    }
+    @Test
+    public void Test_getSubArray3(){
+        int [][]arr = {
+                        {1,     2,      3,      4,      5},
+                        {6,     7,      8,      9,      10},
+                        {11,    12,     13,     14,     15},
+                        {16,    17,     18,     19,     20}};
+        assertArrayEquals(new Array20().getSubArray(arr,1,1,3,5),null);
+    }
+    @Test
+    public void Test_getSubArray4(){
+        int [][]arr = {
+                        {1,     2,      3,      4,      5},
+                        {6,     7,      8,      9,      10},
+                        {11,    12,     13,     14,     15},
+                        {16,    17,     18,     19,     20}};
+        assertArrayEquals(new Array20().getSubArray(arr,1,1,5,5),null);
+    }
+    @Test
+    public void Test_getSubArray5(){
+        int[][] arr = {
+                        {1,     2,      3,      4,      5},
+                        {6,     7,      8,      9,      10},
+                        {11,    12,     13,     14,     15},
+                        {16,    17,     18,     19,     20}};
+        assertArrayEquals(new Array20().getSubArray(arr,1,1,2,3),new int[][]{{7,8,9},{12,13,14}});
+    }
+
 }
