3b3f2c82082d3c9d43fe29389ab309a722b403a2
Minh Hung <ngominhhung97@gmail.com>
2019-07-10 10:21:01 +0700

    Test cov

--- src/test/java/Array19Test.java
+++ src/test/java/Array19Test.java
@@ -15,6 +15,22 @@ public class Array19Test {
                             {1,     2,      3,      4,      5},
                             {12,    13,     14,     15,     6},
                             {11,    10,     9,      8,      7}});
+        assertArrayEquals(new Array19().sortSpiralMatrix(new int[][]{
+                        {1,     3,      2},
+                        {6,     4,      5},
+                        {9,     8,      7},
+                        {10,    12,     11}}),
+                new int[][]{
+                        {1,     2,      3},
+                        {10,    11,     4},
+                        {9,     12,     5},
+                        {8,     7,      6}});
+        assertArrayEquals(new Array19().sortSpiralMatrix(new int[][]{
+                        {2,     3},
+                        {4,     1},}),
+                new int[][]{
+                        {1,     2},
+                        {4,     3}});
         assertArrayEquals(new Array19().sortSpiralMatrix(new int[][]{}),new int[][]{});
         assertArrayEquals(new Array19().sortSpiralMatrix(new int[][]{
                                                 {5,     2,      4,      1,      3}}),
