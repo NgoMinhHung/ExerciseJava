7ffa8d1160dd1a64de0b91b1f089be86e92bca27
Minh Hung <ngominhhung97@gmail.com>
2019-07-10 10:35:10 +0700

    Test cov

--- src/test/java/Array14Test.java
+++ src/test/java/Array14Test.java
@@ -10,6 +10,9 @@ public class Array14Test {
     @Test
     public void Test_convertTwoDimensionalArraytoOneDimensionalArray(){
         assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(3,5, new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}}), new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,10,15,14,13,12,11,6,7,8,9)));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(3,2, new int[][]{{1,2},{3,4},{5,6}}), new ArrayList<Integer>(Arrays.asList(1,2,4,6,5,3)));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(2,2, new int[][]{{1,2},{3,4}}), new ArrayList<Integer>(Arrays.asList(1,2,4,3)));
+        assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(2,3, new int[][]{{1,2,3},{4,5,6}}), new ArrayList<Integer>(Arrays.asList(1,2,3,6,5,4)));
         assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(0,5, new int[][]{{}}), new ArrayList<Integer>(Arrays.asList()));
         assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(3,0, new int[][]{{}}), new ArrayList<Integer>(Arrays.asList()));
         assertEquals(new Array14().convertTwoDimensionalArraytoOneDimensionalArray(0,0, new int[][]{{}}), new ArrayList<Integer>(Arrays.asList()));
