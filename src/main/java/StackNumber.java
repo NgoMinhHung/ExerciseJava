773e23ec4ced76fd1dee965c65095f5cd239ae64
Minh Hung <ngominhhung97@gmail.com>
2019-07-12 10:20:47 +0700

    OOP Exercise 4

---
+++ src/main/java/StackNumber.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class StackNumber<T> {
+
+    private ArrayList<T> stackNumber = new ArrayList<>();
+
+    public boolean isEmpty(){
+        return stackNumber.isEmpty();
+    }
+
+    public void push(T number){
+        stackNumber.add(number);
+    }
+
+    public T pop(){
+        if (isEmpty()){
+            return null;
+        }else{
+            int topIndex = stackNumber.size() - 1;
+            T result = stackNumber.get(topIndex);
+            stackNumber.remove(result);
+            return result;
+        }
+    }
+
+}
---
+++ src/test/java/StackNumberTest.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackNumberTest {
+
+    @Test
+    public void Test_push() {
+        StackNumber<Integer> result = new StackNumber<>();
+        assertTrue(result.isEmpty());
+        result.push(9);
+        assertFalse(result.isEmpty());
+
+    }
+
+    @Test
+    public void Test_pop() {
+        StackNumber<Integer> result = new StackNumber<>();
+        assertNull(result.pop());
+        result.push(9);
+        result.push(5);
+        assertEquals((int) result.pop(), 5);
+    }
+
+    @Test
+    public void Test_isEmpty() {
+        StackNumber<Integer> result = new StackNumber<>();
+        assertTrue(result.isEmpty());
+    }
+}
