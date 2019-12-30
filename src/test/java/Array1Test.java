24e71076a6fbe188b285ed332cfce392382dfbbf
Minh Hung <ngominhhung97@gmail.com>
2019-12-30 20:29:36 +0700

    bla bla

--- src/test/java/Array1Test.java
+++ src/test/java/Array1Test.java
@@ -12,6 +12,7 @@ public class Array1Test {
     public void Test_getDuplicateElements(){
         assertEquals(new Array1().getDuplicateElements(new int[]{1,3,5,6,3,7,9,9,5}),new ArrayList<>(Arrays.asList(3,5,9)));
         assertEquals(new Array1().getDuplicateElements(new int[]{1,3,6,3,4,7,9,4,6,10,54}), new ArrayList<>(Arrays.asList(3,4,6)));
+        assertEquals(new Array1().getDuplicateElements(new int[]{1,5,6,7,9,9,5}),new ArrayList<>(Arrays.asList(5,9)));
     }
 
 }
